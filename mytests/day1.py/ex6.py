278ff278cd285993018a0007e5644da7b1448489
unknown <narjesalmasri330@gmail.com>
2026-09-10 06:48:01 +0300

    ex6

---
+++ mytests/day1.py/ex6.py
@@ -0,0 +1,12 @@
+# Ceiling: per-IP breakdown of failed logins, top 3
+import re
+from collections import Counter
+
+counter = Counter()
+with open("sample_auth.log") as f:
+    for line in f:
+        if "Failed password" in line:
+            ip = re.search(r"\d+\.\d+\.\d+\.\d+",line)
+            if ip:
+                counter[ip.group()] +=1
+print("top 3 ips :",counter.most_common(3))            
