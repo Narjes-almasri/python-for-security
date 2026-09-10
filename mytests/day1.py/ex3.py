db1232f248482f970b8f5a9583c135299be1d67e
unknown <narjesalmasri330@gmail.com>
2026-09-10 05:03:51 +0300

    ex3

--- mytests/day1.py/ex3.py
+++ mytests/day1.py/ex3.py
@@ -1,6 +1,9 @@
 # regular expression module for pattern matching
 # Technique 3: extract a pattern (an IP address) with re.search()
+
+########### notes about re.search() :
+# it return object of Matched thing that containes info like where it found what it found and so on 
+# so we use .group() in order to just get the thing we are searching for without th extra info
+
 import re
 line = "2026-02-10 08:04:59 WARNING Connection retry to 172.16.0.9" 
-ip = re.search(r"\d+\.\d+\.\d+\.\d+",line)
-print ("ip = ", ip)
